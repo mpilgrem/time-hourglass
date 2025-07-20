aa74f72a2fbe1970c427c0513b232c6b1e6abe75
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-07-20 23:59:02 +0100

    Replace `parseTime` with `parseTimeM True`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,3 +11,5 @@ and this project adheres to the
 * Rename `hourglass-0.2.12` package as `time-hourglass-0.2.12`.
 * Change maintainer field to `Mike Pilgrem <public@pilgrem.com>`.
 * Reset `CHANGELOG.md`.
+* In test-suite `test-hourglass` replace use of `parseTime` (removed from
+  package `time-1.10`) with `parseTimeM True`.
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -146,7 +146,7 @@ arithmeticTestSubRef = map testRefToElapsedP
   where ms v = v * 1000000
 
 testRefToElapsedP :: ((Int64, Int64), (Int64, Int64), (Int64, Int64)) -> (ElapsedP, ElapsedP, ElapsedP)
-testRefToElapsedP (a, b, c) = (tupleToElapsedP a, tupleToElapsedP b, tupleToElapsedP c) 
+testRefToElapsedP (a, b, c) = (tupleToElapsedP a, tupleToElapsedP b, tupleToElapsedP c)
   where
     tupleToElapsedP :: (Int64, Int64) -> ElapsedP
     tupleToElapsedP (s, n) = ElapsedP (Elapsed $ Seconds s) (NanoSeconds n)
@@ -210,7 +210,7 @@ tests knowns = testGroup "hourglass"
         [ testProperty "iso8601 date" $ \(e :: Elapsed) ->
             let fmt = calTimeFormatTimeISO8601 (elapsedToPosixTime e)
                 ed1  = localTimeParseE ISO8601_Date fmt
-                md2  = T.parseTime T.defaultTimeLocale fmt "%F"
+                md2  = T.parseTimeM True T.defaultTimeLocale fmt "%F"
              in case (ed1,md2) of
                     (Left err, Nothing)       -> error ("both cannot parse: " ++ show fmt ++ " hourglass-err=" ++ show err)
                     (Left err, Just _)        -> error ("error parsing string: " ++ show err)
