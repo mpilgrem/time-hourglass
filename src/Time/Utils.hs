0690a1db0b50fb98e23fbebad67ee7ac9fe860e7
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-07-28 21:36:02 +0100

    Fix #10 Add `Format_DayYear3` (and `"JJJ"`)

--- src/Time/Utils.hs
+++ src/Time/Utils.hs
@@ -10,30 +10,42 @@ Some padding / formatting functions.
 
 module Time.Utils
   ( pad2
+  , pad3
   , pad4
   , padN
   ) where
 
--- | Pad a number to 2 digits.
+-- | Pad a non-negative integer modulo 100 to 2 digits.
 pad2 :: (Show a, Ord a, Num a, Integral a) => a -> String
 pad2 v
-  | v >= 100   = pad2 (v `mod` 100)
-  | v >= 10    = show v
-  | otherwise  = '0' : show v
+  | v < 0     = undefined
+  | v >= 100  = pad2 (v `mod` 100)
+  | v >= 10   = show v
+  | otherwise = '0' : show v
 
--- | Pad a number to 4 digits.
+-- | Pad a non-negative integer less than 1,000 to 3 digits.
+pad3 :: (Show a, Ord a, Num a, Integral a) => a -> String
+pad3 v
+  | v >= 1000 = undefined
+  | v >= 100  = show v
+  | v >= 10   = '0' : show v
+  | otherwise = '0':'0': show v
+
+-- | Pad a non-negative integer to at least 4 digits.
 pad4 :: (Show a, Ord a, Num a, Integral a) => a -> String
 pad4 v
-  | v >= 1000  = show v
-  | v >= 100   = '0' : show v
-  | v >= 10    = '0':'0' : show v
-  | otherwise  = '0':'0':'0': show v
+  | v < 0 = undefined
+  | v >= 1000 = show v
+  | v >= 100  = '0' : show v
+  | v >= 10   = '0':'0' : show v
+  | otherwise = '0':'0':'0': show v
 
--- | Pad a number to at least N digits.
+-- | Pad a non-negative integer to at least N digits.
 --
 -- If the number is greater, no truncation happens.
 padN :: (Show a, Ord a, Num a, Integral a) => Int -> a -> String
 padN n v
+  | v < 0     = undefined
   | vlen >= n = vs
   | otherwise = replicate (n - vlen) '0' ++ vs
  where
