e62ec88991fd97ea7dffdf2d748e239513546947
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-07-30 23:34:58 +0100

    Aim to optimise `dateTimeFromUnixEpoch`

--- src/Time/Calendar/FromElapsed.hs
+++ src/Time/Calendar/FromElapsed.hs
@@ -23,80 +23,67 @@ import           Time.Types
 
 dateTimeFromUnixEpoch :: Elapsed -> DateTime
 dateTimeFromUnixEpoch (Elapsed (Seconds secs)) =
-  if secs >=0
+  if secs >= 0
     then afterUnixEpoch secs
     else beforeUnixEpoch (negate secs)
  where
 
   isleap :: Int -> Bool
-  isleap y =
+  isleap !y =
     let isDivBy n = y `mod` n == 0
-    in   isDivBy 4 && not (isDivBy 100 && not (isDivBy 400))
+    in   isDivBy 4 && not (isDivBy 100 || isDivBy 400)
+  {-# INLINE isleap #-}
 
   days :: Int -> Int64
-  days y = if isleap y then 366 else 365
+  days !y = if isleap y then 366 else 365
+  {-# INLINE days #-}
 
   monthDays :: Bool -> Int -> Int64
-  monthDays _  0 = 31
-  monthDays isLeap  1 = if isLeap then 29 else 28
-  monthDays _  2 = 31
-  monthDays _  3 = 30
-  monthDays _  4 = 31
-  monthDays _  5 = 30
-  monthDays _  6 = 31
-  monthDays _  7 = 31
-  monthDays _  8 = 30
-  monthDays _  9 = 31
-  monthDays _ 10 = 30
-  monthDays _ 11 = 31
-  monthDays _ _  = undefined
+  monthDays !isLeap !m = months !! m
+   where
+    months = if isLeap then leapMonths else nonLeapMonths
+    leapMonths = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    nonLeapMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  {-# INLINE monthDays #-}
+
+  yearLoop :: Int -> Int -> Int64 -> (Int, Int64)
+  yearLoop !dir !y !d =
+    let yd = days y
+    in  if d <= yd
+          then (y, d)
+          else yearLoop dir (y + dir) (d - yd)
+
+  monthLoop :: Bool -> Int -> Int -> Int64 -> (Int,  Int64, Int64)
+  monthLoop !isLeap !dir !m !d =
+    let md = monthDays isLeap m
+    in  if d <= md
+          then (m, d, md)
+          else monthLoop isLeap dir (m + dir) (d - md)
 
   afterUnixEpoch :: Int64 -> DateTime
   afterUnixEpoch secs1 =
-    let (d1, s1) = secs1 `divMod` 86_400
-        (y, d2) = loop1 1970 d1
-        (m, d) = loop2 (isleap y) 0 d2
-        (h, s2) = s1 `divMod` 3_600
-        (mi, s) = s2 `divMod` 60
+    let (!y, !m, !d, _, !h, !mi, !s) = fromUnixEpoch 1970 0 1 secs1
         date = Date y (toEnum m) (fromIntegral d + 1)
         tod = TimeOfDay (Hours h) (Minutes mi) (Seconds s) 0
     in  DateTime date tod
-   where
-    loop1 :: Int -> Int64 -> (Int, Int64)
-    loop1 y d =
-      let yd = days y
-      in  if d <= yd
-            then (y, d)
-            else loop1 (y + 1) (d - yd)
-
-    loop2 :: Bool -> Int -> Int64 -> (Int,  Int64)
-    loop2 isLeap m d =
-      let md = monthDays isLeap m
-      in  if d <= md
-            then (m, d)
-            else loop2 isLeap (m + 1) (d - md)
 
   beforeUnixEpoch :: Int64 -> DateTime
-  beforeUnixEpoch secs2 =
-    let (d1, s1) = secs2 `divMod` 86_400
-        (y, d2) = loop1 1969 d1
-        (m, d) = loop2 (isleap y) 11 d2
-        (h, s2) = s1 `divMod` 3_600
-        (mi, s) = s2 `divMod` 60
-        date = Date y (toEnum m) (fromIntegral d)
+  beforeUnixEpoch secs1 =
+    let (!y, !m, !d, !md, !h, !mi, !s) = fromUnixEpoch 1969 11 (-1) secs1
+        date = Date y (toEnum m) (fromIntegral $ md - d)
         tod = TimeOfDay (Hours (23 - h)) (Minutes (59 - mi)) (Seconds (60 - s)) 0
     in  DateTime date tod
-   where
-    loop1 :: Int -> Int64 -> (Int, Int64)
-    loop1 y d =
-      let yd = days y
-      in  if d <= yd
-            then (y, d)
-            else loop1 (y - 1) (d - yd)
 
-    loop2 :: Bool -> Int -> Int64 -> (Int, Int64)
-    loop2 isLeap m d =
-      let md = monthDays isLeap m
-      in  if d <= md
-            then (m, md - d)
-            else loop2 isLeap (m - 1) (d - md)
+  fromUnixEpoch ::
+       Int
+    -> Int
+    -> Int
+    -> Int64
+    -> (Int, Int, Int64, Int64, Int64, Int64, Int64)
+  fromUnixEpoch !baseYear !baseMonth !dir !secs1 =
+    let (!d1, !s1) = secs1 `quotRem` 86_400
+        (!y, !d2) = yearLoop dir baseYear d1
+        (!m, !d, !md) = monthLoop (isleap y) dir baseMonth d2
+        (!h, !s2) = s1 `quotRem` 3_600
+        (!mi, !s) = s2 `quotRem` 60
+    in  (y, m, md, d, h, mi, s)
