3d227f5735dde8fb3e8f30b38e5a3f16c4707d92
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-07-31 12:51:28 +0100

    Better algorithm

--- src/Time/Calendar/FromElapsed.hs
+++ src/Time/Calendar/FromElapsed.hs
@@ -23,67 +23,41 @@ import           Time.Types
 
 dateTimeFromUnixEpoch :: Elapsed -> DateTime
 dateTimeFromUnixEpoch (Elapsed (Seconds secs)) =
-  if secs >= 0
-    then afterUnixEpoch secs
-    else beforeUnixEpoch (negate secs)
+  dateTimeFromGPEpoch (secs + 719_468 * 86_400)
+
+dateTimeFromGPEpoch :: Int64 -> DateTime
+dateTimeFromGPEpoch secs =
+  let (!d1, !s1) = secs `quotRem` 86_400
+      (!q400, !r400) = d1 `quotRem` 146_097
+      (!q100, !r100) = r400 `minDivMod` 36_524
+      (!q4, !r4) = r100 `quotRem` 1461
+      (!q1, !r1) = r4 `minDivMod` 365
+      !y' = fromIntegral $ 400 * q400 + 100 * q100 + 4 * q4 + q1
+      !d2 = fromIntegral r1
+      !isLeap = q1 == 0 && (q4 /= 0 || q100 == 0)
+      (!m', !d) = monthDays isLeap d2
+      (!h, !s2) = s1 `quotRem` 3_600
+      (!mi, !s) = s2 `quotRem` 60
+      (y, m) = if m' > 9 then (y' + 1, m' - 10) else (y', m' + 2)
+      !date = Date y (toEnum m) d
+      !tod = TimeOfDay (Hours h) (Minutes mi) (Seconds s) 0
+  in  DateTime { dtDate = date, dtTime = tod }
  where
 
-  isleap :: Int -> Bool
-  isleap !y =
-    let isDivBy n = y `mod` n == 0
-    in   isDivBy 4 && not (isDivBy 100 || isDivBy 400)
-  {-# INLINE isleap #-}
+  minDivMod :: Int64 -> Int64 -> (Int64, Int64)
+  minDivMod !n !d =
+    let (q, r) = n `quotRem` d
+    in  if q == 4 then (3, r + d) else (q, r)
+  {-# INLINE minDivMod #-}
 
-  days :: Int -> Int64
-  days !y = if isleap y then 366 else 365
-  {-# INLINE days #-}
-
-  monthDays :: Bool -> Int -> Int64
-  monthDays !isLeap !m = months !! m
+  monthDays :: Bool -> Int -> (Int, Int)
+  monthDays !isLeap !doy = go 0 table
    where
-    months = if isLeap then leapMonths else nonLeapMonths
-    leapMonths = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    nonLeapMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    table = if isLeap then leapTable else nonLeapTable
+    go !i (a : b : rest)
+      | doy < b   = (i, doy - a + 1)
+      | otherwise = go (i + 1) (b : rest)
+    go _ _ = (11, doy - table !! 11 + 1)  -- fallback for Dec
+    nonLeapTable = [0, 31, 61, 92, 122, 153, 184, 214, 245, 275, 306, 337]
+    leapTable   = [0, 31, 62, 93, 123, 154, 185, 215, 246, 276, 307, 338]
   {-# INLINE monthDays #-}
-
-  yearLoop :: Int -> Int -> Int64 -> (Int, Int64)
-  yearLoop !dir !y !d =
-    let yd = days y
-    in  if d <= yd
-          then (y, d)
-          else yearLoop dir (y + dir) (d - yd)
-
-  monthLoop :: Bool -> Int -> Int -> Int64 -> (Int,  Int64, Int64)
-  monthLoop !isLeap !dir !m !d =
-    let md = monthDays isLeap m
-    in  if d <= md
-          then (m, d, md)
-          else monthLoop isLeap dir (m + dir) (d - md)
-
-  afterUnixEpoch :: Int64 -> DateTime
-  afterUnixEpoch secs1 =
-    let (!y, !m, !d, _, !h, !mi, !s) = fromUnixEpoch 1970 0 1 secs1
-        date = Date y (toEnum m) (fromIntegral d + 1)
-        tod = TimeOfDay (Hours h) (Minutes mi) (Seconds s) 0
-    in  DateTime date tod
-
-  beforeUnixEpoch :: Int64 -> DateTime
-  beforeUnixEpoch secs1 =
-    let (!y, !m, !d, !md, !h, !mi, !s) = fromUnixEpoch 1969 11 (-1) secs1
-        date = Date y (toEnum m) (fromIntegral $ md - d)
-        tod = TimeOfDay (Hours (23 - h)) (Minutes (59 - mi)) (Seconds (60 - s)) 0
-    in  DateTime date tod
-
-  fromUnixEpoch ::
-       Int
-    -> Int
-    -> Int
-    -> Int64
-    -> (Int, Int, Int64, Int64, Int64, Int64, Int64)
-  fromUnixEpoch !baseYear !baseMonth !dir !secs1 =
-    let (!d1, !s1) = secs1 `quotRem` 86_400
-        (!y, !d2) = yearLoop dir baseYear d1
-        (!m, !d, !md) = monthLoop (isleap y) dir baseMonth d2
-        (!h, !s2) = s1 `quotRem` 3_600
-        (!mi, !s) = s2 `quotRem` 60
-    in  (y, m, md, d, h, mi, s)
