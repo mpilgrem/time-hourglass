f8e9f26aa7bb001eded420598534991990a033f4
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-08-02 19:59:56 +0100

    Add a further benchmark for dateTimeFromUnixEpoch

--- benchmarks/FromEpoch.hs
+++ benchmarks/FromEpoch.hs
@@ -4,13 +4,18 @@ module Main
   ( main
   ) where
 
-import           Time.Calendar ( dateTimeFromUnixEpoch )
+import qualified Time.Calendar as C
+import qualified Time.Calendar.FromElapsed as FE
 import           Test.Tasty.Bench ( bench, bgroup, defaultMain, nf )
 
 main :: IO ()
 main = defaultMain
   [ bgroup  "fromEpoch (Time.Calendar)" $
-      [ bench "2024-03-01" $ nf dateTimeFromUnixEpoch 1_709_337_599
-      , bench "2024-02-29" $ nf dateTimeFromUnixEpoch 1_709_251_199
+      [ bench "2024-03-01" $ nf C.dateTimeFromUnixEpoch 1_709_337_599
+      , bench "2024-02-29" $ nf C.dateTimeFromUnixEpoch 1_709_251_199
+      ]
+  , bgroup  "fromEpoch (Time.Calendar.FromElapsed)" $
+      [ bench "2024-03-01" $ nf FE.dateTimeFromUnixEpoch 1_709_337_599
+      , bench "2024-02-29" $ nf FE.dateTimeFromUnixEpoch 1_709_251_199
       ]
   ]
